0b308efa0ce7adcbfabe1db8b32320b2c4601e99
2001Marty <martin.prasil.2001@gmail.com>
2022-01-04 23:54:45 +0100

    commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,35 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '0122317177msh2d501a8878fc66cp19d791jsnb19631647c12'
+  }
+
+const baseUrl =  'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+          query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+    
+        getCryptoDetails: builder.query({
+          query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+    
+        // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coinmepe/${coinId}/history?tiriod=${timeperiod}`
+        getCryptoHistory: builder.query({
+          query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+        }),
+      }),
+    });
+    
+    export const {
+      useGetCryptosQuery,
+      useGetCryptoDetailsQuery,
+      useGetCryptoHistoryQuery,
+    } = cryptoApi;
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,25 @@
+import {createApi, CreateApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '0122317177msh2d501a8878fc66cp19d791jsnb19631647c12'
+  }
+
+const baseUrl ='https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoNewsHeaders});
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (buileder) => ({
+        getCryptoNews: buileder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        })
+    })
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
+
+
